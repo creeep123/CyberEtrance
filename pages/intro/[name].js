6ae8d92a521fc9b1058684909f9b7273211d6366
YUMING MA <DavidYangPersonal@outlook.com>
2021-07-23 14:59:39 +0800

    【内容页】动态路由和静态生成的预渲染

--- pages/intro/[name].js
+++ pages/intro/[name].js
@@ -2,18 +2,13 @@
 import utilStyles from '../../styles/utils.module.css';
 import { useRouter } from 'next/router';
 import Layout from '../../components/layout';
-import { findProject } from '../../components/constant';
+import { findProject, projects } from '../../components/constant';
 
-const Intro = () => {
-  const router = useRouter();
-  const { name } = router.query;
-  debugger;
-  const project = findProject(name);
-
+const Intro = ({ project }) => {
   return (
     <Layout>
       <article className={utilStyles.article}>
-        <h1 className={utilStyles.headingXl}>{name}</h1>
+        <h1 className={utilStyles.headingXl}>{project.name}</h1>
         <div className={utilStyles.lightText}>
           {/* <Date dateString={postData.date} /> */}
           2020 02 13
@@ -23,5 +18,32 @@ const Intro = () => {
     </Layout>
   );
 };
+
+export async function getStaticProps({ params }) {
+  // 调用外部 API 获取博文列表
+  const project = findProject(params.name);
+
+  // 通过返回 { props: { posts } } 对象，Blog 组件
+  // 在构建时将接收到 `posts` 参数
+  return {
+    props: {
+      project,
+    },
+  };
+}
+
+// 此函数在构建时被调用
+export async function getStaticPaths() {
+  // 调用外部 API 获取博文列表
+
+  // 据博文列表生成所有需要预渲染的路径
+  const paths = projects.map((project) => ({
+    params: { name: project.NAME },
+  }));
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false };
+}
 
 export default Intro;
